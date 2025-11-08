762db74511f97d450a240b1feeb8c4d712e67aba
ngmtuan07 <ngmtuan0711@gmail.com>
2025-11-08 14:48:44 -0500

    hello

--- Utilities/vimrc.cpp
+++ Utilities/vimrc.cpp
@@ -1,17 +1,13 @@
 source $VIMRUNTIME/defaults.vim
-
 let mapleader = " "
-
 runtime! ftplugin/man.vim
 packadd! matchit
-
 set modeline
 set autoread
 set hidden
 set undofile
 set undodir=~/.vim/undo//
 set noswapfile
-
 set tabstop=4
 set softtabstop=4
 set shiftwidth=4
@@ -20,13 +16,10 @@ set smarttab
 set autoindent
 set nojoinspaces
 set nostartofline
-
 set hlsearch
 set ignorecase
 set smartcase
-
 set mouse=a
-
 set background=dark
 set number
 set laststatus=2
@@ -34,11 +27,10 @@ set belloff=all
 set splitbelow
 set splitright
 set scrolloff=2
-
 nnoremap Y y$
 nnoremap <C-L> :nohlsearch<Bar>normal! <C-L><CR>
 nnoremap <Leader>h :tab h 
 nnoremap <Leader>e :e $MYVIMRC<CR>
 nnoremap <Leader>r :source $MYVIMRC<CR>
-nnoremap <Leader>c <Cmd>w | !g++-std=c++20 -O2 -Wall % -o %:r<CR>
-nnoremap <Leader>x <Cmd>!./%:r<CR>
+nnoremap <Leader>c :w <Bar> !g++ -std=c++20 -O2 -Wall % -o %:r<CR>
+nnoremap <Leader>x :!./%:r<CR>
