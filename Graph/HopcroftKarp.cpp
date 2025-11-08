c7a88b8876038ddde069a38d9cd8fc7e7654f69f
ngmtuan07 <ngmtuan0711@gmail.com>
2025-11-08 13:23:51 -0500

    hello

--- Graph/HopcroftKarp.cpp
+++ Graph/HopcroftKarp.cpp
@@ -1,61 +1,56 @@
 struct maximum_bipartite_matching {
-  int n, m;
-  vector<int> matchX, matchY, dist;
-  vector<vector<int>> g;
-  int matched;
-  maximum_bipartite_matching(int _n, int _m)
-      : n(_n), m(_m), matchX(n + 1, -1), matchY(m + 1, -1), dist(n + 1, -1), g(n + 1), matched(0) {}
-
-  void add_edge(int u, int v) { g[u].push_back(v); }
-  void bfs() {
-    queue<int> q;
-    for (int i = 0; i < n; ++i) {
-      if (matchX[i] == -1)
-        q.push(i), dist[i] = 0;
-      else
-        dist[i] = -1;
+    int n, m, turn, matched;
+    vector<int> matchR, matchL, vis;
+    vector< vector<int> > adj;
+    vector< pair<int, int> > edges;
+    maximum_bipartite_matching(int _n, int _m): n(_n), m(_m), matchL(n + 1), matchR(m + 1, -1), vis(n + 1), adj(n + 1), turn(0), matched(0) {
     }
-    while (!q.empty()) {
-      int u = q.front();
-      q.pop();
-      for (int v : g[u]) {
-        if (matchY[v] != -1 and dist[matchY[v]] == -1) {
-          dist[matchY[v]] = dist[u] + 1;
-          q.push(matchY[v]);
+
+    void add_edge(int u, int v) {
+        edges.push_back({u, v});
+    }
+
+    bool dfs(int u) {
+        if (vis[u] == turn) return false;
+        vis[u] = turn;
+
+        for(int v: adj[u]) {
+            if (matchR[v] == -1 || dfs(matchR[v])) {
+                matchR[v] = u;
+                return true;
+            }
         }
-      }
+        return false;
     }
-  }
-  bool dfs(int u) {
-    for (int v : g[u]) {
-      if (matchY[v] == -1) {
-        matchX[u] = v, matchY[v] = u;
-        return 1;
-      }
-    }
-    for (int v : g[u]) {
-      if (dist[matchY[v]] == dist[u] + 1 and dfs(matchY[v])) {
-        matchX[u] = v, matchY[v] = u;
-        return 1;
-      }
-    }
-    return 0;
-  }
 
-  void match() {
-    while (1) {
-      bfs();
-      int augment = 0;
-      for (int i = 0; i < n; ++i)
-        if (matchX[i] == -1) augment += dfs(i);
-      if (!augment) break;
-      matched += augment;
+    void match() {
+        shuffle(edges.begin(), edges.end(), rng);
+        for(pair<int, int> edge: edges) {
+            adj[edge.first].push_back(edge.second);
+        }
+        bool flag = 1;
+
+        while(flag) {
+            flag = 0;
+            ++turn;
+            for(int u = 0; u < n; ++u) {
+                if (matchL[u])
+                    continue;
+                if (dfs(u)) {
+                    flag = matchL[u] = true;
+                    ++matched;
+                }
+            }
+        }
     }
-  }
-  vector<pii> get_edges() {
-    vector<pii> res;
-    for (int i = 0; i < n; ++i)
-      if (matchX[i] != -1) res.push_back({i, matchX[i]});
-    return res;
-  }
+
+    vector< pair<int, int> > get_edges() {
+        vector< pair<int, int> > res;
+        for(int v = 0; v < m; ++v) {
+            if (matchR[v] != -1) {
+                res.push_back({matchR[v], v});
+            }
+        }
+        return res;
+    }
 };
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # icpc-notebook
 https://github.com/pin3da/notebook-generator
+notebook-generator . --size 8 --columns 3 --initials University\ of\ Massachusetts\ Amherst
